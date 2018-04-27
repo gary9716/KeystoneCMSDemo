7219fb0aedc692ef64432191ddb0ad3539368a63
Kuan-Ting Chou <gary9716@gmail.com>
2018-04-27 19:00:36 +0900

    update

--- linux-setup/setup-stunnel.sh
+++ linux-setup/setup-stunnel.sh
@@ -5,4 +5,5 @@ sudo cp ./stunnel.conf /etc/stunnel/
 #generate stunnel.pem
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095
+sudo su
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
