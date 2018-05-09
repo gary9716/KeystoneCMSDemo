4daaa657f1db9aad3a0f6f1953989c9c74a50f9b
Kuan-Ting Chou <gary9716@gmail.com>
2018-05-09 17:04:08 +0900

    fix keystone.sh

--- linux-setup/init-keystone.sh
+++ linux-setup/init-keystone.sh
@@ -11,7 +11,7 @@
 
 dir="/home/riceserver001/KeystoneCMSDemo"
 script="keystone.js"
-sudo alias forever="/home/riceserver001/.nvm/versions/node/v8.11.1/bin/forever"
+alias forever="/home/riceserver001/.nvm/versions/node/v8.11.1/bin/forever"
 cmd="forever restart $script"
 user=""
 
@@ -36,16 +36,7 @@ case "$1" in
 		/etc/init.d/mongod restart && /etc/init.d/redis restart
         echo "Starting $name"
         cd "$dir"
-        if [ -z "$user" ]; then
-            sudo $cmd >> "$stdout_log" 2>> "$stderr_log" &
-        else
-            sudo -u "$user" $cmd >> "$stdout_log" 2>> "$stderr_log" &
-        fi
-        echo $! > "$pid_file"
-        if ! is_running; then
-            echo "Unable to start, see $stdout_log and $stderr_log"
-            exit 1
-        fi
+        forever restart $script
     fi
     ;;
     stop)
