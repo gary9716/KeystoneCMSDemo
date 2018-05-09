eddd9cd202bb5913204d6a28d4edb3a29c2a4852
Kuan-Ting Chou <gary9716@gmail.com>
2018-05-09 16:37:20 +0900

    add scripts

--- linux-setup/copy-init-scripts-and-config.sh
+++ linux-setup/copy-init-scripts-and-config.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 PARENT_DIR=$(dirname $(readlink -f $0))/../
 cd $PARENT_DIR
+
 sudo cp mongod-linux.conf /etc/mongod.conf
 sudo cp ./linux-setup/init-mongod.sh /etc/init.d/mongod
 sudo chmod +x /etc/init.d/mongod
 
 sudo cp ./linux-setup/init-redis-server.sh /etc/init.d/redis
 sudo chmod +x /etc/init.d/redis
+
+sudo cp ./linux-setup/init-keystone.sh /etc/init.d/keystone
+sudo chmod +x /etc/init.d/keystone
