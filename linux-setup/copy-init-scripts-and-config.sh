08bbb31b152215681e918df4e900a06bcb05a93b
Kuan-Ting Chou <gary9716@gmail.com>
2018-05-09 16:22:59 +0900

    fix mongod-linux.conf

--- linux-setup/copy-init-scripts-and-config.sh
+++ linux-setup/copy-init-scripts-and-config.sh
@@ -3,7 +3,7 @@ PARENT_DIR=$(dirname $(readlink -f $0))/../
 cd $PARENT_DIR
 sudo cp mongod-linux.conf /etc/mongod.conf
 sudo cp ./linux-setup/init-mongod.sh /etc/init.d/mongod
-chmod +x /etc/init.d/mongod
+sudo chmod +x /etc/init.d/mongod
 
 sudo cp ./linux-setup/init-redis-server.sh /etc/init.d/redis
-chmod +x /etc/init.d/redis
+sudo chmod +x /etc/init.d/redis
