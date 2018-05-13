9f405480726a64d226f49a2403b278db8ace1c1e
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2018-05-13 16:40:08 +0900

    complete interval-acc-rec.js

--- pdfmake-docs/interval-acc-recs.js
+++ pdfmake-docs/interval-acc-recs.js
@@ -1,6 +1,18 @@
 var moment = require('moment');
 var keystone = require('keystone');
-
+var Constants = require(__base + 'Constants');
+var opTypeMap =
+{
+    'create':'開戶' ,
+    'deposit':'入款' ,
+    'withdraw':'提款' ,
+    'annuallyWithdraw':'年度結清',
+    'unfreeze':'解凍',
+    'freeze':'凍結' ,
+    'transact':'兌領' ,
+    'close':'結清' ,
+    'accUserChange':'過戶' 
+};
 const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -35,11 +47,11 @@ module.exports = function(req, res) {
         columns: [
             {
                 text: "農民姓名:" + form.farmer.name,
-                width: "30%"
+                width: "20%"
             },
             {
                 text: "資料期間:" + intervalInfo,
-                width: '40%'
+                width: '50%'
             },
             {
                 text: '印表日期:' + moment().rocFormat(),
@@ -63,90 +75,96 @@ module.exports = function(req, res) {
     };
 
     var footerFunc = function(page, pages) {
-        return [
-            //divisionInfo,
-            {
-                alignment: 'center',
-                text: { text: '第' + page + '頁' },
-            }
-        ];
+        return {
+            alignment: 'center',
+            text: { text: '第' + page + '頁' }
+        };
 
     };
     //-----------
+    var accountRecList = keystone.list(Constants.AccountRecordListName);
 
-
-    //main content
-    var accRecsAgg = form.accRecsAgg;
-    var accAgg = form.accAgg;
-    var balanceSum = accAgg.balanceSum? accAgg.balanceSum : 0;
-
-    var tableBody;
-    if(accRecsAgg) {
-        if(form.tag === 'all') {
-            tableBody = [
-                ['開戶次數', accRecsAgg.create? accRecsAgg.create.count:0, '結清次數', accRecsAgg.close? accRecsAgg.close.count:0],
-                ['凍結次數', accRecsAgg.freeze? accRecsAgg.freeze.count:0, '解凍次數', accRecsAgg.unfreeze? accRecsAgg.unfreeze.count:0],
-                ['入款次數', accRecsAgg.deposit? accRecsAgg.deposit.count:0, '入款總金額', accRecsAgg.deposit? numberWithCommas(accRecsAgg.deposit.amount):0],
-                ['提款次數', accRecsAgg.withdraw? accRecsAgg.withdraw.count:0, '提款總金額', accRecsAgg.withdraw? numberWithCommas(accRecsAgg.withdraw.amount):0],
-                ['兌領次數', accRecsAgg.transact? accRecsAgg.transact.count:0, '兌領總金額', accRecsAgg.transact? numberWithCommas(accRecsAgg.transact.amount):0],
-                ['年度結清戶數', accRecsAgg.annuallyWithdraw? accRecsAgg.annuallyWithdraw.count:0,'年度結清總金額', accRecsAgg.annuallyWithdraw? numberWithCommas(accRecsAgg.annuallyWithdraw.amount):0],
-                ['凍結中戶數', accAgg.freeze? accAgg.freeze.count:0,'凍結戶總金額', accAgg.freeze? numberWithCommas(accAgg.freeze.balance):0],
-                ['有效戶數', accAgg.unfreeze? accAgg.unfreeze.count:0,'有效戶總金額', accAgg.unfreeze? numberWithCommas(accAgg.unfreeze.balance):0],
-                ['全部戶數', accAgg.all? accAgg.all.count:0,'全部未領總金額', accAgg.all? numberWithCommas(accAgg.all.balance):0],  
-            ]  
-        }
-        else {
-            tableBody = [
-                ['凍結中戶數', accAgg.freeze? accAgg.freeze.count:0,'凍結戶總金額', accAgg.freeze? numberWithCommas(accAgg.freeze.balance):0],
-                ['有效戶數', accAgg.unfreeze? accAgg.unfreeze.count:0,'有效戶總金額', accAgg.unfreeze? numberWithCommas(accAgg.unfreeze.balance):0],
-                ['全部戶數', accAgg.all? accAgg.all.count:0,'全部未領總金額', accAgg.all? numberWithCommas(accAgg.all.balance):0],  
-            ]
-        }
-        
-
-        tableBody.forEach(function(textArray) {
-            textArray[1] = {
-                text: textArray[1],
-                alignment: 'right'
-            }
-            textArray[3] = {
-                text: textArray[3],
-                alignment: 'right'
-            }
-        });
+    var applyDateFilter = false;
+    var dateFilters = {};
+    if(form.startDate) {
+        dateFilters['$gte'] = form.startDate;
+        applyDateFilter = true;
     }
-    
-    var tableContent = {
-        widths: [120, '*', 120,'*'], //width can be [number, *, auto]
-        body: tableBody? tableBody:[]
+
+    if(form.endDate) {
+        dateFilters['$lt'] = form.endDate;
+        applyDateFilter = true;
+    }
+        
+    var filters = {
+        account: form._id,
     };
 
-    var doc = {
-        // a string or { width: number, height: number }
-        pageSize: 'A4',
+    if(applyDateFilter) {
+        filters['date'] = dateFilters;
+    }
 
-        // by default we use portrait, you can change it to landscape if you wish
-        pageOrientation: 'portrait',
-        
-        // [left, top, right, bottom] or [horizontal, vertical] or just a number for equal margins
-        pageMargins: [ 40, 40, 40, 250 + offset*2 ],
-        
-        defaultStyle: {
-            font: 'msjh'
-        },
-        content: [
-            { text: locationName, fontSize: 18, alignment: 'center' },
-            { text: sheetName , fontSize: 14, alignment: 'center' },
-            metaInfo,
-            {
-                margin: [0, 5, 0, 5],
-                table: tableContent
+    return accountRecList.model.find(filters)
+    .populate('operator period')
+    .lean()
+    .exec()
+    .then(function(accRecs) {
+        //main content
+        var tableBody = [
+            ['日期','類別','金額','對口帳戶','期別','操作員'] //header
+        ];
+
+        accRecs.forEach(function(accRec) {
+            var dateStr = moment(accRec.date).format('YYYY/MM/DD');
+            var rowOfData = [ 
+                dateStr, 
+                opTypeMap[accRec.opType], 
+                accRec.amount, 
+                accRec.ioAccount? accRec.ioAccount : '', 
+                accRec.period? accRec.period.name : '', 
+                accRec.operator? accRec.operator.name : ''
+            ];
+            //console.log(rowOfData);
+            tableBody.push(rowOfData);
+        });
+
+        var tableContent = {
+            // headers are automatically repeated if the table spans over multiple pages
+            // you can declare how many rows should be treated as headers
+            headerRows: 1,
+            widths: [ 70, 60, 90, '*', 80, 60],
+            body: tableBody? tableBody : [['']]
+        };
+
+        var doc = {
+            // a string or { width: number, height: number }
+            pageSize: 'A4',
+
+            // by default we use portrait, you can change it to landscape if you wish
+            pageOrientation: 'portrait',
+            
+            // [left, top, right, bottom] or [horizontal, vertical] or just a number for equal margins
+            pageMargins: [ 40, 40, 40, 40 ],
+            
+            defaultStyle: {
+                font: 'msjh'
             },
-        ],
+            content: [
+                { text: locationName, fontSize: 18, alignment: 'center' },
+                { text: sheetName , fontSize: 14, alignment: 'center' },
+                metaInfo,
+                {
+                    //layout: 'lightHorizontalLines', // optional
+                    table: tableContent,
+                    margin: [ 0, 5]
+                },
+            ],
 
-        footer: footerFunc
-    };
+            footer: footerFunc
+        };
 
-    return doc;
+        return doc;
+
+    });
 
 }
